fa65e352bfcc0591bd0168c65802bc753862cdab
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-02-06 02:07:48 +0000

    update test for length getter method works as expected given no arguments

--- data_structures/linked_lists/__test__/singly_linked_list.test.ts
+++ data_structures/linked_lists/__test__/singly_linked_list.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "bun:test";
-import { SinglyLinkedListNode } from "../singly_linked_list_node";
+// import { SinglyLinkedListNode } from "../singly_linked_list_node";
 import { SinglyLinkedList } from "../singly_linked_list";
 
 describe("Singly Linked List", () => {
@@ -8,5 +8,8 @@ describe("Singly Linked List", () => {
     expect(
       singly_linked_list.head === null && singly_linked_list.tail === null
     ).toBe(true);
+  });
+  test("Length of list should be zero as no arguments were given upon instantiation", () => {
+    expect(singly_linked_list.length === 0).toBe(true);
   });
 });
