ed47e70a955517ca62e53be9d2beae60b1e7adee
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-02-06 01:37:18 +0000

    update singly linked list node test file updated the test description in the file as it was not accurate to what the test was actually doing

--- data_structures/linked_lists/__test__/singly_linked_list_node.test.ts
+++ data_structures/linked_lists/__test__/singly_linked_list_node.test.ts
@@ -6,13 +6,13 @@ describe("Singly Linked List Node", () => {
   const node1 = new SinglyLinkedListNode("node");
   const node2 = new SinglyLinkedListNode({ name: "Finch", age: 20 });
   const node3 = new SinglyLinkedListNode(5);
-  test("Should create a new singly linked list with some data", () => {
+  test("Should be a singly linked list node with some data", () => {
     expect(node1.data).toBe("node");
   });
-  test("Should create a new singly linked list with some data", () => {
+  test("Should be a singly linked list node with some data", () => {
     expect(node2.data).toStrictEqual({ name: "Finch", age: 20 });
   });
-  test("Should create a new singly linked list with some data", () => {
+  test("Should be a singly linked list node with some data", () => {
     expect(node3.data).toBe(5);
   });
 });
