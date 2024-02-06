c550375c7bb51e5eb5c61a80a0273432d411f400
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-02-06 01:36:22 +0000

    add singly linked list test file file to test that the singly linked list operates as expected so far and it does

---
+++ data_structures/linked_lists/__test__/singly_linked_list.test.ts
@@ -0,0 +1,12 @@
+import { describe, test, expect } from "bun:test";
+import { SinglyLinkedListNode } from "../singly_linked_list_node";
+import { SinglyLinkedList } from "../singly_linked_list";
+
+describe("Singly Linked List", () => {
+  const singly_linked_list = new SinglyLinkedList();
+  test("Should be a linked list with a head and taill that are both null", () => {
+    expect(
+      singly_linked_list.head === null && singly_linked_list.tail === null
+    ).toBe(true);
+  });
+});
