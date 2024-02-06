5539a9941b2a91e6c17e3b7d553e8f40f492a9dc
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-02-06 04:40:18 +0000

    update test to account for multiple nodes ran into edge case where adding node instances did not work as expected however upon debugging the issue is clear and will be amended as of the next commit. Testing the "prepending" and "appending" of literals works as expected and all properties update as expected

--- data_structures/linked_lists/__test__/singly_linked_list.test.ts
+++ data_structures/linked_lists/__test__/singly_linked_list.test.ts
@@ -1,8 +1,8 @@
 import { describe, test, expect } from "bun:test";
-// import { SinglyLinkedListNode } from "../singly_linked_list_node";
+import { SinglyLinkedListNode } from "../singly_linked_list_node";
 import { SinglyLinkedList } from "../singly_linked_list";
 
-describe("Singly Linked List", () => {
+describe("Singly Linked List with no nodes", () => {
   const singly_linked_list = new SinglyLinkedList();
   test("Should be a linked list with a head and taill that are both null", () => {
     expect(
@@ -11,5 +11,36 @@ describe("Singly Linked List", () => {
   });
   test("Length of list should be zero as no arguments were given upon instantiation", () => {
     expect(singly_linked_list.length === 0).toBe(true);
+  });
+});
+
+describe("Singly linked list with one node", () => {
+  const singly_linked_list = new SinglyLinkedList();
+  let node1 = new SinglyLinkedListNode("first");
+  singly_linked_list.prepend(node1);
+
+  test("Length of list should be one as there is one node", () => {
+    expect(singly_linked_list.length === 1).toBe(true);
+  });
+  test("List head should be equal to the tail", () => {
+    expect(singly_linked_list.head === singly_linked_list.tail).toBe(true);
+  });
+});
+
+describe("Singly linked list with multiple nodes", () => {
+  const singly_linked_list = new SinglyLinkedList();
+  let node1 = new SinglyLinkedListNode("first");
+  let node2 = new SinglyLinkedListNode("second");
+  singly_linked_list.append("first");
+  singly_linked_list.prepend("second");
+
+  test("Length of list should not be one as there are multiple nodes", () => {
+    expect(singly_linked_list.length === 1).toBe(false);
+  });
+  test("List head should not be equal to the tail", () => {
+    expect(singly_linked_list.head === singly_linked_list.tail).toBe(false);
+  });
+  test("List tail should be latest apended value", () => {
+    expect(singly_linked_list.tail).toStrictEqual(node1);
   });
 });
