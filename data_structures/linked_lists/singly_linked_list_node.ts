c49d5ec9101876d148304d8ecf70da46bd25451c
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-02-07 10:01:39 +0000

    refactor for type safety and encapsulation with help from github copilot I refacotred this file to have type safety for the list nodes and to have the "data" and "next" variables private so they cannot be modified outside of the class

--- data_structures/linked_lists/singly_linked_list_node.ts
+++ data_structures/linked_lists/singly_linked_list_node.ts
@@ -4,18 +4,70 @@
     - A pointer to the next node
 
     I am going to have the singly linked list node be strongly
-    types so that the pointer to the next node must be of type
+    typed so that the pointer to the next node must be of type
     singly linked list node or null (null since the node itself 
     might be the tail and will have to point to null)
 */
 
-export type SinglyLinkedListNodeNext = SinglyLinkedListNode | null;
+/*
+      I am going to update the singly linked list node to have:
+      - Type safety for the data 
+      - Encapsulation for the data and the pointer to the next node
+        - The data and next node will be private and only accessible through methods
 
-export class SinglyLinkedListNode {
-  data: any;
-  next: SinglyLinkedListNodeNext;
-  constructor(data: any, next: SinglyLinkedListNodeNext = null) {
-    this.data = data;
-    this.next = next;
+      e.g.
+
+      const node1 = new SinglyLinkedListNode<string>("node");
+      const node2 = new SinglyLinkedListNode<object>({ name: "Finch", age: 20 });
+      const node3 = new SinglyLinkedListNode<number>(5);
+      console.log(node1.data); // "node"
+      console.log(node2.data); // { name: "Finch", age: 20 }
+      console.log(node3.data); // 5
+      console.log(node1.next); // null
+      console.log(node2.next); // null
+      console.log(node3.next); // null
+      node1.data = "new node";
+      console.log(node1.data); // "new node"
+      node1.next = node2;
+      console.log(node1.next); // { data: "new node", next: SinglyLinkedListNode { data: { name: "Finch", age: 20 }, next: null }}
+*/
+
+export type SinglyLinkedListNodeNext<T> = SinglyLinkedListNode<T> | null;
+
+export class SinglyLinkedListNode<T, U = T> {
+  #data: T;
+  #next: SinglyLinkedListNodeNext<U>;
+  constructor(data: T, next: SinglyLinkedListNodeNext<U> = null) {
+    this.#data = data;
+    this.#next = next;
+  }
+  get data(): T {
+    return this.#data;
+  }
+  set data(value: T) {
+    this.#data = value;
+  }
+  get next(): SinglyLinkedListNodeNext<U> {
+    return this.#next;
+  }
+  set next(value: SinglyLinkedListNodeNext<U>) {
+    this.#next = value;
   }
 }
+
+const node1 = new SinglyLinkedListNode<string>("node");
+const node2 = new SinglyLinkedListNode<string, object>({
+  name: "Finch",
+  age: 20,
+});
+const node3 = new SinglyLinkedListNode<number>(5);
+console.log(node1.data); // "node"
+console.log(node2.data); // { name: "Finch", age: 20 }
+console.log(node3.data); // 5
+console.log(node1.next); // null
+console.log(node2.next); // null
+console.log(node3.next); // null
+node1.data = "new node";
+console.log(node1.data); // "new node"
+node1.next = node2;
+console.log(node1.next); // { data: "new node", next: SinglyLinkedListNode { data: { name: "Finch", age: 20 }, next: null }}
