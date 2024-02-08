8c109d449af85f3785509a02148cab4559af1208
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-02-08 11:49:27 +0000

    add doubly linked list class and basic "append", "prepend" and length getter methods tested and works as expected

---
+++ data_structures/linked_lists/Doubly/doubly_linked_list.ts
@@ -0,0 +1,52 @@
+import { DoublyLinkedListNode } from "./doubly_linked_list_node";
+import type { DoublyLinkedListNodeShape } from "./doubly_linked_list_node";
+
+export class DoublyLinkedList<T> {
+  #head: DoublyLinkedListNodeShape<T> | null;
+  #tail: DoublyLinkedListNodeShape<T> | null;
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+  }
+  get length(): number {
+    let length: number = 0;
+    let current: DoublyLinkedListNodeShape<T> | null = this.#head;
+    while (current) {
+      length += 1;
+      current = current.next;
+    }
+    return length;
+  }
+  get head(): DoublyLinkedListNodeShape<T> | null {
+    return this.#head;
+  }
+  get tail(): DoublyLinkedListNodeShape<T> | null {
+    return this.#tail;
+  }
+  inputCheker(data: T | DoublyLinkedListNodeShape<T>) {
+    const actualData = data instanceof DoublyLinkedListNode ? data.data : data;
+    return new DoublyLinkedListNode(actualData);
+  }
+  prepend(data: T | DoublyLinkedListNode<T>) {
+    const newNode = this.inputCheker(data);
+    if (!this.#head) {
+      this.#head = this.#tail = newNode;
+      return this;
+    }
+    newNode.next = this.#head;
+    this.#head = newNode;
+    return this;
+  }
+  append(data: T | DoublyLinkedListNode<T>) {
+    const newNode = this.inputCheker(data);
+    if (!this.#head) {
+      this.#head = this.#tail = newNode;
+      return this;
+    }
+    if (this.#tail) {
+      this.#tail.next = newNode;
+      this.#tail = newNode;
+      return this;
+    }
+  }
+}
